64211fb4a08ce84110197a79e29ef82e507170d5
lauraAlabau <arlaura3@gmail.com>
2024-06-15 21:38:45 +0200

    add float components

---
+++ src/components/floatComponents/Contact.tsx
@@ -0,0 +1,13 @@
+export const Contact = () => {
+  return (
+    <div className="fixed bottom-0 right-auto z-10 w-10 left-10 text-slate-100 ">
+      <div className="flex flex-col m-0 p-2.5 list-none align-middle gap-5  after:block after:w-[1px] after:h-24 after:bg-slate-100 after:mx-auto">
+        <div className="transform -rotate-90">
+          <a href="mailto:laura.alabau.rodriguez@gmail.com ">
+            laura.alabau.rodriguez@gmail.com
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
