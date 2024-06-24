ed9f1034e866c8509281ae109045c3be5fb5d7fe
lauraAlabau <arlaura3@gmail.com>
2024-06-24 21:18:32 +0200

    fix: temporary link to old page projects

--- src/pages/AllProjects.tsx
+++ src/pages/AllProjects.tsx
@@ -1,3 +1,4 @@
+import { BiLinkExternal } from "react-icons/bi";
 import { ProjectList } from "../components/projectList/ProjectList";
 import { PROJECTS_DATA } from "../utils/projects_data";
 
@@ -5,9 +6,22 @@ const AllProjects = () => {
   return (
     <main className="max-w-[1600px] mx-auto my-0 text-slate-100 lg:py-52 lg:px-36 h-auto  min-h-screen">
       <header>
-        <h1 className="m-0 font-semibold leading-tight text-7xl">Archive</h1>
-        <p className="text-base text-red-300">
+        <h1 className="mt-28 text-3xl ml-2.5 lg:ml-0 font-semibold leading-tight lg:block lg:text-7xl">
+          Archive
+        </h1>
+        <p className="ml-2.5 lg:ml-0 text-base text-red-300">
           All the projects I’ve worked on
+        </p>
+        <p className="lg:flex ml-2.5 lg:ml-0 text-base text-slate-300 ">
+          This page is WIP, you can check my old projects in this
+          <a
+            href="https://laura-alabau-rodriguez.com/"
+            className="inline-flex items-center gap-1 ml-2 font-bold hover:text-red-300"
+            aria-label="Go to my old projects page"
+          >
+            Link
+            <BiLinkExternal className="w-4 h-4" />
+          </a>
         </p>
       </header>
       <div className="my-8 lg:my-24">
